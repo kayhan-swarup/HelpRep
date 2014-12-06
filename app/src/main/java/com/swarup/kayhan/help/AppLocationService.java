40d5a102875634dd30528230f78e33e60cf44198
Kayhan Temp <kayhan9204@gmail.com>
2014-12-07 05:06:50 +0600

    location service

--- app/src/main/java/com/swarup/kayhan/help/AppLocationService.java
+++ app/src/main/java/com/swarup/kayhan/help/AppLocationService.java
@@ -11,15 +11,7 @@ import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.IBinder;
-import android.util.Log;
-import android.view.View;
-import android.widget.Toast;
 
-import com.google.android.gms.location.LocationServices;
-
-import java.io.IOException;
-import java.util.List;
-import java.util.Locale;
 
 /**
  * Created by Kayhan on 11/26/2014.
@@ -29,62 +21,28 @@ public class AppLocationService extends Service implements LocationListener {
     protected LocationManager locationManager;
     Location location;
 
-
-    public String getAddressString() {
-        return addressString;
-    }
-
-    public void setAddressString(String addressString) {
-        this.addressString = addressString;
-    }
-
-    String addressString="";
     private static final long MIN_DISTANCE_FOR_UPDATE = 10;
     private static final long MIN_TIME_FOR_UPDATE = 1000 * 60 * 2;
 
+    public AppLocationService(Context context) {
+        locationManager = (LocationManager) context
+                .getSystemService(LOCATION_SERVICE);
+    }
 
     public Location getLocation(String provider) {
         if (locationManager.isProviderEnabled(provider)) {
             locationManager.requestLocationUpdates(provider,
                     MIN_TIME_FOR_UPDATE, MIN_DISTANCE_FOR_UPDATE, this);
             if (locationManager != null) {
-                Location location = locationManager.getLastKnownLocation(provider);
+                location = locationManager.getLastKnownLocation(provider);
                 return location;
             }
         }
         return null;
     }
 
-    String TAG="TAG";
     @Override
-    public void onLocationChanged(Location loc) {
-        MainActivity.progressBar.setVisibility(View.INVISIBLE);
-        Toast.makeText(
-                getBaseContext(),
-                "Location changed : Lat: " + loc.getLatitude() + " Lng: "
-                        + loc.getLongitude(), Toast.LENGTH_SHORT).show();
-        String longitude = "Longitude: " + loc.getLongitude();
-        Log.v(TAG, longitude);
-        String latitude = "Latitude: " + loc.getLatitude();
-        Log.v(TAG, latitude);
-
-        /*----------to get City-Name from coordinates ------------- */
-        String cityName = null;
-        Geocoder gcd = new Geocoder(getBaseContext(), Locale.getDefault());
-        List<Address> addresses;
-        try {
-            addresses = gcd.getFromLocation(loc.getLatitude(),
-                    loc.getLongitude(), 1);
-            if (addresses.size() > 0)
-                System.out.println(addresses.get(0).getLocality());
-            cityName = addresses.get(0).getLocality();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        String s = longitude + "\n" + latitude + "\n\nMy Currrent City is: "
-                + cityName;
-        setAddressString(s);
+    public void onLocationChanged(Location location) {
     }
 
     @Override
